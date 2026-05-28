3eab4e544bed43927397d3ae78eafd2da0499c31
Sipan <60195494+RoyalArm001@users.noreply.github.com>
2026-05-28 09:06:17 +0000

    auto update 2026-05-28 09:06:17

--- Start SmartTech.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\start-smarttech.ps1"
-echo.
-echo SmartTech server stopped. Press any key to close this window.
-pause >nul
